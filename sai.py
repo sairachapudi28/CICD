612ba8897dd1c1befb455aedfa10eca6e68c07a7
sairachapudi28 <52847780+sairachapudi28@users.noreply.github.com>
2023-05-19 11:52:33 +0530

    Update sai.py

--- sai.py
+++ sai.py
@@ -1,5 +1,10 @@
-def greet():
-    print("Hello, Docker!")
+from flask import Flask
 
-if __name__ == "__main__":
-    greet()
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Docker!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
