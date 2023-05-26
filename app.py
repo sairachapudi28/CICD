a61351238f76249f8b9d5ab1d5d9d92400aae56e
sairachapudi28 <52847780+sairachapudi28@users.noreply.github.com>
2023-05-26 11:09:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def hello():
     return "This is Sai"
 
-if __name__ == '__main__':
-    # Use Gunicorn as the WSGI server
+if __name__ == '__main__'
+
     app.run(host= '0.0.0.0' , port = 8000)
