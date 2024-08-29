8458d8f4afd328d4fcfe37871c855c83d8462e64
sairachapudi28 <52847780+sairachapudi28@users.noreply.github.com>
2024-08-29 11:03:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "This is Sai Rachapudi. v1.1.0"
+def hello_world():
+    return jsonify({'message': 'Hello this is V1.0'})
 
 if __name__ == '__main__':
-
-    app.run(host= '0.0.0.0',port = 9000)
+    app.run(host='0.0.0.0', port=5000)
